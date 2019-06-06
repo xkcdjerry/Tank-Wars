244285d86addd90348678ca07c86435a4e82a3b1
Jerry Jackson <49294431+xkcdjerry@users.noreply.github.com>
2019-06-06 18:08:44 +0800

    A fully functioning branch(1)
    Every time a branch is functioning,it is merged into the main branch.

---
+++ PUBG 2.0.0 （unfinished）.py
@@ -0,0 +1,35 @@
+﻿import random
+import time
+import os
+import pickle
+
+import pygame
+
+from classes import *
+
+
+pygame.init()
+
+
+def main():
+    stop = False
+    game = Game((MID,))
+    while not stop:
+        try:
+            stop, fps_real = game.run()
+            frame_ratio = FPS/fps_real
+
+        except BaseException:
+            # makes sure it's shut down right
+            pygame.quit()
+            raise
+        print('RATIO:', frame_ratio, 'FPS:', fps_real)
+        game.new()
+        game.window.blit(BG, (0, 0))
+    game.stop()
+    return game
+
+
+if __name__ == '__main__':
+    cached_game = main()
+    # for testing
---
+++ virtual.py
@@ -0,0 +1,53 @@
+import os
+
+import pygame
+
+BASE = "Bin"
+WIDTH = HIGHT = None
+SMMOTH = True
+
+
+def debug():
+    print(WIDTH, HIGHT, SMMOTH)
+
+
+def config(width, hight, smmoth=None):
+    global WIDTH, HIGHT, SMMOTH
+    WIDTH = width
+    HIGHT = hight
+    SMMOTH = SMMOTH if smmoth is None else bool(smmoth)
+
+
+def clear():
+    global WIDTH, HIGHT, SMMOTH
+    WIDTH = HIGHT = None
+    SMMOTH = True
+
+
+def shiftsize(img):
+    rect = img.get_rect()
+    width = rect.width if WIDTH is None else WIDTH
+    hight = rect.height if HIGHT is None else HIGHT
+    func = pygame.transform.smoothscale if SMMOTH else pygame.transform.scale
+    return func(img, (width, hight))
+
+
+def _load(s):
+    return pygame.image.load(transform_name(s))
+
+
+def load(s, ignore_config=False):
+    # debug()
+    img = _load(s)
+    if ignore_config:
+        return img
+    else:
+        return shiftsize(img)
+
+
+def init():
+    return pygame.init()
+
+
+def transform_name(s):
+    return os.path.join(BASE, s)
